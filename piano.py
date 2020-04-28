92f3807c57238d9dbfe83e0dc2ef248e835904dc
Murats Seidovs <murat28022002@gmail.com>
2020-04-28 17:05:14 +0100

    Add files via upload

--- piano.py
+++ piano.py
@@ -2,18 +2,21 @@ import pgzrun
 def on_key_down(key):
     if key == keys.SPACE:
         print("SPACE")
-        music.play_once('Piano11')
+        music.play_once('piano11')
 
     elif key == keys.UP:
         print("UP")
+        music.play_once('piano12')
 
     elif key == keys.DOWN:
         print("DOWN")
+        music.play_once('piano13')
 
     elif key == keys.LEFT:
         print("LEFT")
-
+        music.play_once('piano14')
     elif key == keys.RIGHT:
         print("RIGHT")
+        music.play_once('piano16')
 
 pgzrun.go()
